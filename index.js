0054b48dc4aee3f3534f8b78aac0098bcb2fea7c
rumyantsevat <ipochinili@gmail.com>
2020-09-25 22:36:58 +0300

    onText

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-const TelegramBot = require('node-telegram-bot-api');
-const TOKEN = '794524932:AAGEs0sG8m5htNK10_nCb78Tr5JN_kzwLck';
+// const TelegramBot = require('node-telegram-bot-api');
+// const TOKEN = '794524932:AAGEs0sG8m5htNK10_nCb78Tr5JN_kzwLck';
+//
+// const bot = new TelegramBot(TOKEN, {polling: true});
+// bot.on('message', msg => {
+//     bot.sendMessage(msg.chat.id, `Hello from Heroku, bot says: "Hi, ${msg.from.first_name}"` );
+// });
 
-const bot = new TelegramBot(TOKEN, {polling: true});
-bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, `Hello from Heroku, bot says: "Hi, ${msg.from.first_name}"` );
-})
+const TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '794524932:AAGEs0sG8m5htNK10_nCb78Tr5JN_kzwLck';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
+
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+
+    // send a message to the chat acknowledging receipt of their message
+    bot.sendMessage(chatId, 'Received your message');
+});
+
