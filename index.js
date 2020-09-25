66b38392d6fcf40872a41af18d0db941b4dd3efb
rumyantsevat <ipochinili@gmail.com>
2020-09-25 23:18:34 +0300

    reminder

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '794524932:AAGEs0sG8m5htNK10_nCb78Tr5JN_kzwLck';
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+// const bot = new TelegramBot(token, {polling: true});
+
 
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
@@ -35,4 +36,25 @@ bot.on('message', (msg) => {
     // send a message to the chat acknowledging receipt of their message
     bot.sendMessage(chatId, 'Received your message');
 });
+
+let notes = [];
+bot.onText(/напомни (.+) в (.+)/, function (msg, match) {
+    var userId = msg.from.id;
+    var text = match[1];
+    var time = match[2];
+
+    notes.push({ 'uid': userId, 'time': time, 'text': text });
+
+    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
+});
+
+setInterval(function(){
+    for (var i = 0; i < notes.length; i++) {
+        const curDate = new Date().getHours() + ':' + new Date().getMinutes();
+        if (notes[i]['time'] === curDate) {
+            bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: '+ notes[i]['text'] + ' сейчас.');
+            notes.splice(i, 1);
+        }
+    }
+}, 1000);
 
